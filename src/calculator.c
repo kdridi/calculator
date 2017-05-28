be4e95bbe85b98e8eb79d4d42b1af698868be08c
Karim DRIDI <karim.dridi@gmail.com>
2017-05-28 19:29:57 +0200

    parse from string instead of from stdin

--- src/calculator.c
+++ src/calculator.c
@@ -1,16 +1,25 @@
-	
 #include "calculator.h"
 
 int main(void)
 {
-	int ch;
-
-	ch = 'a';
-	while (ch <= 'z')
-		sym[ch++ - 'a'] = 0;
-
-	yydebug = 0;
-	yyparse();
-
-	return (0);
+    int ch;
+    
+    ch = 'a';
+    while (ch <= 'z')
+        sym[ch++ - 'a'] = 0;
+    
+    yydebug = 0;
+    char string[] =
+        "a=10+2*5\n"
+        "b=99*a+a\n"
+        "a\n"
+        "b\n"
+        "c\n"
+        "";
+    
+    YY_BUFFER_STATE buffer = yy_scan_string(string);
+    yyparse();
+    yy_delete_buffer(buffer);
+    
+    return (0);
 }
--- src/calculator.h
+++ src/calculator.h
@@ -4,7 +4,11 @@
 extern int sym[26];
 extern int yydebug;
 
-int yyparse(void);
-void yyerror(char *s);
+typedef struct yy_buffer_state *YY_BUFFER_STATE;
+
+extern int yyparse();
+extern void yyerror(char *s);
+extern YY_BUFFER_STATE yy_scan_string (char *str);
+extern void yy_delete_buffer (YY_BUFFER_STATE buffer);
 
 #endif // _CALCULATOR_H_
